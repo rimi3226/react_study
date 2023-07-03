5ad7878cffb34e81d57c97ba5b5896beadd55873
Rimi <lora3226@inu.ac.kr>
2023-07-03 23:55:35 +0900

    6. Event

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function Header(props) {
   return (
     <header>
       {/* props */}
-      <h1><a href="/">{props.title}</a></h1>
+      <h1><a href="/" onClick={(event)=>{
+        event.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
     </header>
   )
 }
@@ -14,7 +17,12 @@ function Nav(props) {
   const lis=[]
   for(let i=0; i<props.topics.length;i++){
     let t=props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>);
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}
+      >{t.title}</a></li>);
   }
   return (
     <nav>
@@ -42,11 +50,15 @@ function App() {
   ]
   return (
     <div>
-      <Header title="REACT"></Header>
+      <Header title="REACT" onChangeMode={()=>{
+        alert('Header');
+      }}></Header>
       <Header title="STUDY"></Header>
       <Header title="REACT"></Header>
 
-      <Nav topics={topics}></Nav>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
 
       <Article title="Welcome" body="Hello, Web"></Article>
       <Article title="Hi" body="Hello, React"></Article>
