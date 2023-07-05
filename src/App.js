b544bd5aba470a9933ee7a493807b00dccc37736
Rimi <lora3226@inu.ac.kr>
2023-07-05 10:44:20 +0900

    9. Update

--- src/App.js
+++ src/App.js
@@ -46,17 +46,17 @@ function Article(props) {
 }
 
 //Create
-function Create(props){
-  return(
+function Create(props) {
+  return (
     <article>
       <h2>Create</h2>
-      <form onSubmit={event=>{
+      <form onSubmit={event => {
         event.preventDefault();
         const title = event.target.title.value;
         const body = event.target.body.value;
-        props.onCreate(title,body);
+        props.onCreate(title, body);
       }}>
-        <p><input type="text" name="title" placeholder='title'/></p>
+        <p><input type="text" name="title" placeholder='title' /></p>
         <p><textarea name="body" placeholder='body'></textarea></p>
         <p><input type="submit" value="Create"></input></p>
       </form>
@@ -64,12 +64,37 @@ function Create(props){
   )
 }
 
+//Update
+function Update(props){
+  const [title,setTitle]=useState(props.title);
+  const [body,setBody]=useState(props.body);
+  return (
+    <article>
+      <h2>Update</h2>
+      <form onSubmit={event => {
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onUpdate(title, body);
+      }}>
+        <p><input type="text" name="title" placeholder='title' value={title} onChange={event=>{
+          setTitle(event.target.value);
+        }}/></p>
+        <p><textarea name="body" placeholder='body' value={body} onChange={event=>{
+          setBody(event.target.value);
+        }}></textarea></p>
+        <p><input type="submit" value="Update"></input></p>
+      </form>
+    </article>
+  )
+}
+
 //APP
 function App() {
-  const [mode,setMode]=useState('WELCOME');
-  const [id,setId]=useState(null);
-  const [nextId,setNextId]=useState(4);
-  const [topics,setTopics]=useState([
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     { id: 1, title: 'html', body: 'html is ...' },
     { id: 2, title: 'css', body: 'css is ...' },
     { id: 3, title: 'js', body: 'javascript is ...' }
@@ -78,31 +103,57 @@ function App() {
 
   //모드 설정
   let content = null;
+  let contextControl=null;
   if (mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, Web"></Article>
 
   } else if (mode === 'READ') {
-    let title,body = null;
-    for(let i=0;i<topics.length;i++){
-      if(topics[i].id===id){ //id를 State로 만든 이유. 여기에서 사용하려고
-        title=topics[i].title;
-        body=topics[i].body;
+    let title, body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) { //id를 State로 만든 이유. 여기에서 사용하려고
+        title = topics[i].title;
+        body = topics[i].body;
       }
     }
-    content=<Article title={title} body={body}></Article>
-  } else if (mode === 'CREATE'){
-    content=<Create onCreate={(_title,_body)=>{
-      const newTopic={id:nextId,title:_title, body:_body};
+    content = <Article title={title} body={body}></Article>
+    contextControl= <li><a href={'/update'+id} onClick={event=>{
+      event.preventDefault();
+      setMode('UPDATE');
+    }}>Update</a></li>
+
+  } else if (mode === 'CREATE') {
+    content = <Create onCreate={(_title, _body) => {
+      const newTopic = { id: nextId, title: _title, body: _body };
       //기존 데이터 복제 후 변경
-      const newTopics=[...topics]
+      const newTopics = [...topics]
       newTopics.push(newTopic);
       setTopics(newTopics);
 
       //모드 변경
       setMode('READ');
       setId(nextId);
-      setNextId(nextId+1);
+      setNextId(nextId + 1);
     }}></Create>
+  } else if(mode === 'UPDATE'){
+    let title, body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) { //id를 State로 만든 이유. 여기에서 사용하려고
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content =<Update title={title} body={body} onUpdate={(title,body)=>{
+      const newTopics=[...topics];
+      const updatedTopic={id:id, title:title, body:body};
+      for (let i = 0; i < newTopics.length; i++) {
+        if (topics[i].id === id) { //id를 State로 만든 이유. 여기에서 사용하려고
+          newTopics[i]=updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+      setMode('READ');
+    }}></Update>
   }
 
   return (
@@ -119,10 +170,14 @@ function App() {
       }}></Nav>
 
       {content}
-      <a href='/create' onClick={event=>{
-        event.preventDefault();
-        setMode('CREATE');
-      }}>Create</a>
+      <ul>
+        <li><a href='/create' onClick={event => {
+          event.preventDefault();
+          setMode('CREATE');
+        }}>Create</a></li>
+        {contextControl}
+      </ul>
+        
     </div>
   );
 }
